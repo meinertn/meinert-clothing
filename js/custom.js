26a2a7ba3749c475d5ba7f7cede8d46014bcbff3
meinertn <63525680+meinertn@users.noreply.github.com>
2020-04-12 04:55:39 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -20,6 +20,8 @@
         slides: [
             { src: 'images/slide-image01.jpg' },
             { src: 'images/slide-image02.jpg' }
+            { src: 'images/hoodie-mockup-of-a-young-hipster-guy-staring-down-and-leaning-against-a-wall-a12594 (1).png' },
+            { src: 'images/hispanic-couple-posing-for-a-picture-while-wearing-different-crewneck-sweatshirts-mockup-a15568 (1).png' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
