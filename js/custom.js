6b709e52ce8837f4b1b718c7ce0fe900c2822b74
meinertn <63525680+meinertn@users.noreply.github.com>
2020-04-13 11:39:45 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,13 @@
     $('body').vegas({
         slides: [
             { src: 'images/slide-image01.jpg' },
-            { src: 'images/slide-image02.jpg' }
+            { src: 'images/slide-image02.jpg' },
+            { src: 'unisex-t-shirt-mockup-featuring-a-happy-girl-with-a-trendy-outfit-22962.png' },
+            { src: 'tote-bag-mockup-of-a-cool-guy-sitting-on-a-concrete-fence-29451 (1).png' },
+            { src: 'mockup-of-a-couple-wearing-long-sleeve-t-shirts-in-autumn-31808 (2).png' },
+            { src: 'hoodie-mockup-of-a-young-man-wearing-a-dad-hat-in-an-urban-setting-31178 (3).png' },
+            { src: 'hoodie-mockup-of-a-woman-posing-in-a-tropical-scenario-3552-el1.png' },
+            { src: 'athleisure-mockup-featuring-a-woman-wearing-a-crop-top-hoodie-32466 (3).png' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
