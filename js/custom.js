1c7e2eeb84f660f69f58167144edab0fb2d087bf
meinertn <63525680+meinertn@users.noreply.github.com>
2020-04-13 12:00:32 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -21,11 +21,11 @@
             { src: 'images/slide-image01.jpg' },
             { src: 'images/slide-image02.jpg' },
             { src: 'images/unisex-t-shirt-mockup-featuring-a-happy-girl-with-a-trendy-outfit-22962.png' },
-            { src: 'images/tote-bag-mockup-of-a-cool-guy-sitting-on-a-concrete-fence-29451 (1).png' },
-            { src: 'images/mockup-of-a-couple-wearing-long-sleeve-t-shirts-in-autumn-31808 (2).png' },
-            { src: 'images/hoodie-mockup-of-a-young-man-wearing-a-dad-hat-in-an-urban-setting-31178 (3).png' },
+            { src: 'images/tote-bag-mockup-of-a-cool-guy-sitting-on-a-concrete-fence-29451.png' },
+            { src: 'images/mockup-of-a-couple-wearing-long-sleeve-t-shirts-in-autumn-31808.png' },
+            { src: 'images/hoodie-mockup-of-a-young-man-wearing-a-dad-hat-in-an-urban-setting-31178.png' },
             { src: 'images/hoodie-mockup-of-a-woman-posing-in-a-tropical-scenario-3552-el1.png' },
-            { src: 'images/athleisure-mockup-featuring-a-woman-wearing-a-crop-top-hoodie-32466 (3).png' }
+            { src: 'images/athleisure-mockup-featuring-a-woman-wearing-a-crop-top-hoodie-32466.png' }
         ],
         timer: false,
         transition: [ 'zoomOut', ]
